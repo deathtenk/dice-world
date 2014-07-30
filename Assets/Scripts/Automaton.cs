955cfadce1ca8d202df3fdad636ed7946b63cdf0
Henk Reder <henk@anestuary.com>
2014-07-30 09:47:43 -0400

    making sure things are up to date

--- Assets/Scripts/Automaton.cs
+++ Assets/Scripts/Automaton.cs
@@ -6,34 +6,113 @@ public class Automaton : MonoBehaviour {
 
 	public Cell[] allCells;
 	public Dictionary<Vector3, Cell> listOfCellsByPosition;
-
-	void Update () 
-	{
-	}
+	
 
 	void OnMouseDown()
 	{
 		allCells = FindObjectsOfType(typeof(Cell)) as Cell[];
-		listOfCellsByPosition = ListCellsByPosition();
-		gameObject.BroadcastMessage("DetermineState");
+		listOfCellsByPosition = OrganizeCellsByPosition();
+		Organize(listOfCellsByPosition);
+
 	}
 
-	public Dictionary<Vector3, Cell> ListCellsByPosition()
+	public void Organize(Dictionary <Vector3, Cell> ocbp)
 	{
-		Dictionary<Vector3, Cell> acbp = new Dictionary<Vector3, Cell>();
+
+		foreach (Cell cell in allCells)
+			cell.StateSwitch(ocbp);
+	}
+
+	public Dictionary<Vector3, Cell> OrganizeCellsByPosition()
+	{
+		Dictionary<Vector3, Cell> x = new Dictionary<Vector3, Cell>();
 		foreach (Cell cell in allCells) 
 		{
-			acbp.Add(cell.transform.position, cell);
+			x.Add(cell.transform.position, cell);
 		}
-		return acbp;
+		return x;
 	}
 
-	void OrderedState()
+	public int DetermineState(Dictionary<Vector3, Cell> locbp)
 	{
-		foreach (Cell cell in allCells)
+		
+		Vector3 cp = transform.position;
+		Vector3 s = new Vector3 (cp.x, cp.y, cp.z - 1.0F);
+		Vector3 n = new Vector3 (cp.x, cp.y, cp.z + 1.0F);
+		Vector3 e = new Vector3 (cp.x + 1.0F, cp.y, cp.z);
+		Vector3 w = new Vector3 (cp.x - 1.0F, cp.y, cp.z);
+		Vector3 se = new Vector3 (cp.x + 1, cp.y, cp.z - 1.0F);
+		Vector3 sw = new Vector3 (cp.x - 1.0F, cp.y, cp.z - 1.0F);
+		Vector3 ne = new Vector3 (cp.x - 1.0F, cp.y, cp.z + 1.0F);
+		Vector3 nw = new Vector3 (cp.x + 1.0F, cp.y, cp.z + 1.0F);
+		int t = 0;
+		
+		if (locbp.ContainsKey(s)) 
 		{
-			cell.DetermineState();
+			Cell c = locbp[s];
+			if (c.State == 1)
+			{
+				t++;
+			}
 		}
+		if (locbp.ContainsKey(n)) 
+		{
+			Cell c = locbp[n];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(e)) 
+		{
+			Cell c = locbp[e];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(w)) 
+		{
+			Cell c = locbp[w];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(se)) 
+		{
+			Cell c = locbp[se];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(sw)) 
+		{
+			Cell c = locbp[sw];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(ne)) 
+		{
+			Cell c = locbp[ne];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		if (locbp.ContainsKey(nw)) 
+		{
+			Cell c = locbp[nw];
+			if (c.State == 1)
+			{
+				t++;
+			}
+		}
+		return t;
+		
 	}
 
 }
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,15 +6,13 @@ public class Cell : Automaton {
 
 	private int state;
 	private GameObject auto;
-	private Automaton a;
-	int t = 0;
 
 	void Start () {
 		auto = GameObject.FindGameObjectWithTag("automaton");
 		transform.parent = auto.transform;
-		a = transform.parent.GetComponent<Automaton>();
+		//a = transform.parent.GetComponent<Automaton>();
 		int stateRoll = Random.Range(0, 100);
-		if (stateRoll <= 25)
+		if (stateRoll <= 35)
 			state = 1;
 		else
 			state = 0;
@@ -34,90 +32,7 @@ public class Cell : Automaton {
 	void StateOne()	{
 		renderer.material.color = Color.red;
 	}
-
-	public void DetermineState()
-	{
-
-		Vector3 cp = transform.position;
-		Vector3 s = new Vector3 (cp.x, cp.y, cp.z - 1.0F);
-		Vector3 n = new Vector3 (cp.x, cp.y, cp.z + 1.0F);
-		Vector3 e = new Vector3 (cp.x + 1.0F, cp.y, cp.z);
-		Vector3 w = new Vector3 (cp.x - 1.0F, cp.y, cp.z);
-		Vector3 se = new Vector3 (cp.x + 1, cp.y, cp.z - 1.0F);
-		Vector3 sw = new Vector3 (cp.x - 1.0F, cp.y, cp.z - 1.0F);
-		Vector3 ne = new Vector3 (cp.x - 1.0F, cp.y, cp.z + 1.0F);
-		Vector3 nw = new Vector3 (cp.x + 1.0F, cp.y, cp.z + 1.0F);
-
-		if (a.listOfCellsByPosition.ContainsKey(s)) 
-		{
-			Cell c = a.listOfCellsByPosition[s];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(n)) 
-		{
-			Cell c = a.listOfCellsByPosition[n];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(e)) 
-		{
-			Cell c = a.listOfCellsByPosition[e];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(w)) 
-		{
-			Cell c = a.listOfCellsByPosition[w];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(se)) 
-		{
-			Cell c = a.listOfCellsByPosition[se];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(sw)) 
-		{
-			Cell c = a.listOfCellsByPosition[sw];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(ne)) 
-		{
-			Cell c = a.listOfCellsByPosition[ne];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (a.listOfCellsByPosition.ContainsKey(nw)) 
-		{
-			Cell c = a.listOfCellsByPosition[nw];
-			if (c.State == 1)
-			{
-				t++;
-			}
-		}
-		if (t >= 4)
-			this.State = 1;
-		else
-			this.State = 0;
-
-	}
+	
 
 	public int State 
 	{
@@ -134,6 +49,15 @@ public class Cell : Automaton {
 				case 1: StateOne (); break;
 			}
 		}
+	}
+
+	public void StateSwitch(Dictionary <Vector3, Cell> locbp)
+	{
+		int t = DetermineState(locbp);
+		if (t >= 4)
+			this.State = 1;
+		else
+			this.State = 0;
 	}
 
 }
